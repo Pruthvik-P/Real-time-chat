c6db8b985f1b302bd17cffb3f2f6f3e9daee46f4
Pruthvik <pruthvik6666@gmail.com>
2026-01-19 22:31:19 +0530

    user and chat api

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "../config/http.config";
+import { getUserService } from "../services/user.service";
+
+export const getUsersController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const users = await getUserService(userId);
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Users reterieved successfully",
+      users
+    });
+  },
+);
